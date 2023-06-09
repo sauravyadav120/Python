60da516e79fb67e7a9ae671ec2f07e64124db1e0
sauravyadav120 <125265964+sauravyadav120@users.noreply.github.com>
2023-06-09 11:54:45 +0530

    Added Chapter 1

---
+++ Day-1/Chapter 1(Ans).py
@@ -0,0 +1,36 @@
+#print("Hello World")
+
+#import os
+#print("Saurav Kumar")
+
+#import flask    #flask use the web create
+#print("sky")
+
+#Author : Saurav Yadav  ( Single line comment)
+''' I am check my mobile'''
+#import os
+#print("SAURAV YADAV")
+
+# chapter 1 - Practice set
+#import os
+#print('''Twinkle, twinkle, little star,
+#How I wonder what you are!
+#Up above the world so high,
+#Like a diamond in the sky.''')
+
+#Useing for Read Evaluate Print Loop(REPL)
+#a = 10
+#b = 5
+#c = a+b
+#print(c)
+
+#from playsound import playsound
+#playsound('C:\\Users\\user\\Desktop\\Py.Course\\play.mp3')
+
+#import os
+#print(os.listdir())
+
+# Author : SAURAV KUMAR 
+# Location : GAMMA 2
+# import os
+# print(os.listdir())
